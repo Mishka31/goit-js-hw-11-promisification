7974a2e97df9507669a6ee2fe187ac889ead361d
Misha Krasnonos <muzishutka7@gmail.com>
2021-07-01 16:47:33 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -6,42 +6,13 @@ const users = [
   { name: "Ajax", active: true },
   { name: "Lux", active: false },
 ];
-
-const toggleUserState = (allUsers, userName, callback) => {
-  const updatedUsers = allUsers.map((user) =>
-    user.name === userName ? { ...user, active: !user.active } : user
-  );
-
-  callback(updatedUsers);
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((resolve) => {
+    resolve(allUsers.map((user) => (user.name === userName ? { ...user, active: !user.active } : user)));
+  });
 };
 
 const logger = (updatedUsers) => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-toggleUserState(users, "Mango", logger);
-toggleUserState(users, "Lux", logger);
-
-/*
- * Должно работать так
- */
-// toggleUserState(users, "Mango").then(logger);
-// toggleUserState(users, "Lux").then(logger);
-
-// const makePromise = (text, delay) => {
-//   return new Promise((resolve) => {
-//     setTimeout(() => resolve(text), delay);
-//   });
-// };
-
-// const promiseA = makePromise("promiseA", 1000);
-// const promiseB = makePromise("promiseB", 3000);
-
-// /*
-//  * Выполнится спустя 3 секунды, когда выполнится второй промис с задержкой в 3c.
-//  * Первый выполнится через секунду и просто будет готов
-//  */
-// Promise.race([promiseA, promiseB])
-//   .then((result) => console.log(result)) //["promiseA", "promiseB"]
-//   .catch((err) => console.log(err));
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
